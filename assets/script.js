f503b693ee7deb19424a7a6fe741371fb3366dce
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2021-02-20 11:51:07 -0800

    polished up script.js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,37 @@
 const APIKEY = "485a33a929a1db2dc1031920b9f05a3a"
 const queryURL = "https://api.openweathermap.org/data/2.5/weather?appid=" + APIKEY + "&q=" + citySearch + "&units=imperial";
 
+const cityList = [];
+const searchedCity;
+
+
+
+//main page forecast information
+$("#currentDate").text("Today " + moment().format('ddd Do'));
+
+for (let i = 0; i < 5; i++) {
+
+    const startForecast = i + 1;
+    const forecastCard = $(`div[data-card|="${i}"]`);
+
+    forecastCard.html(`<h4> ${moment().add(startForecast, 'days').format('ddd')} </h4>`)
+};
+
+// (bad code, bad code, Wat you gon' do) Wat you gon' do when city search is submitted (foo'?)
+$(document).ready(function () {
+
+    $(`#city-search`).submit(function () {
+        event.preventDefault();
+        searchedCity = $('#city-text').val().trim();
+        currentWeather(searchedCity);
+        getIndex(searchedCity);
+        getForecast(searchedCity);
+        addHistory(searchedCity);
+    })
+});
+
+
+//function to GET the current weather
 function currentWeather(citySearch) {
 
     $.ajax({
@@ -27,6 +58,7 @@ function currentWeather(citySearch) {
     })
 }
 
+//function to grab the Latitude x Longitude of a location.
 function getIndex(citySearch) {
 
     $.ajax({
@@ -68,62 +100,64 @@ function getIndex(citySearch) {
 
 }
 
-//search function
-const cityList = [];
 
 
+
+// if there's no cities in local storage than load new-default cityList
+// else grab the local storage cities and do work on them
 if (localStorage.getItem('Cities') === null) {
-
+    
+    // new-default cityList
     cityList = ["Atlanta", "Chicago", "Austin", "New York", "San Francisco", "Portland", "Seattle"]
-
+    //stringify them cities
     localStorage.setItem('Cities', JSON.stringify(cityList));
-
+    //KOBE the strings
     cityList.forEach(element => {
         $(`$searchHistory`).append(`
         <li class="searchItem">${element}</li>
         `);
     });
-
+    // execute functions to tell user what's up with the weather(it's the sky.)
     currentWeather(cityList[0]);
     getIndex(cityList[0]);
     getForecast(cityList[0])
 
 } else {
-
+    // parse the local storage cities
     cityList = JSON.parse(localStorage.getItem('Cities'));
-
+    //KOBE them
     cityList.forEach(element => {
 
         $('#searchHistory').append(`
-        
+            <li class="searchItem">${element}</li>
         `)
-
-    })
+    });
+        // THE SKY
+        currentWeather(cityList[0]);
+        getIndex(cityList[0]);
+        getForecast(cityList[0]);
 }
+// when .searchitem is clicked execute this event function
+$('.searchItem').on('click', function (event) {
+    // the target of the innextText is now itemText
+    const itemText = event.target.innerText;
+    //ItemText is used as the value to execute
+    $('city-text').val(itemText);
+    currentWeather(itemText);
+    getIndex(itemText);
+    getForecast(itemText);
 
-const searchedCity;
-
-$(document).ready(function () {
-
-    $(`#city-search`).submit(function () {
-        event.preventDefault();
-        searchedCity = $('#city-text').val().trim();
-        currentWeather(searchedCity);
-        getIndex(searchedCity);
-        getForecast(searchedCity);
-        addHistory(searchedCity);
-    })
 });
 
-//main page forecast
-$("#currentDate").text("Today " + moment().format('ddd Do'));
+// It puts the search history on the page or it gets the lotion again.
+function addHistory() {
 
-for (let i = 0; i < 5; i++) {
+    $('#searchHistory').prepend(`
+        <li class="searchItem">${searchedCity}</li>
+    `);
 
-    const startForecast = i + 1;
-    const forecastCard = $(`div[data-card|="${i}"]`);
+        cityList.unshift(searchedCity);
 
-    forecastCard.html(`<h4> ${moment().add(startForecast, 'days').format('ddd')} </h4>`)
+        localStorage.setItem('Cities', JSON.stringify(cityList));
 };
-
 
