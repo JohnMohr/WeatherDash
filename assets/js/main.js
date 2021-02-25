2b56aca69be5132165c0e15e517ef56ee2e97d9f
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2021-02-25 13:01:13 -0800

    splitting script file into manageable chunks

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+//main page forecast information
+$("#currentDate").text("Today " + moment().format('ddd'));
+
+for (let i = 0; i < 5; i++) {
+
+    const startForecast = i + 1;
+    const forecastCard = $(`div[data-card|="${i}"]`);
+
+    forecastCard.html(`<h4> ${moment().add(startForecast, 'days').format('ddd')} </h4>`)
+};
+
+const APIKEY = "485a33a929a1db2dc1031920b9f05a3a"
+
+let searchedCity;
+
+// (bad code, bad code, Wat you gon' do) Wat you gon' do when city search is submitted (foo'?)
+$(document).ready(function () {
+
+    $(`#city-search`).submit(function () {
+        event.preventDefault();
+        searchedCity = $('#city-text').val().trim();
+        currentWeather(searchedCity);
+        getIndex(searchedCity);
+        getForecast(searchedCity);
+        addHistory(searchedCity);
+    })
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,12 @@
-const APIKEY = "485a33a929a1db2dc1031920b9f05a3a"
+
 const queryURL = "https://api.openweathermap.org/data/2.5/weather?appid=" + APIKEY + "&q=" + citySearch + "&units=imperial";
 
 let cityList = [];
-//main page forecast information
-$("#currentDate").text("Today " + moment().format('ddd Do'));
 
-for (let i = 0; i < 5; i++) {
-
-    const startForecast = i + 1;
-    const forecastCard = $(`div[data-card|="${i}"]`);
-
-    forecastCard.html(`<h4> ${moment().add(startForecast, 'days').format('ddd')} </h4>`)
-};
 
      
 
-// (bad code, bad code, Wat you gon' do) Wat you gon' do when city search is submitted (foo'?)
-$(document).ready(function () {
-    let searchedCity;
 
-    $(`#city-search`).submit(function () {
-        event.preventDefault();
-        searchedCity = $('#city-text').val().trim();
-        currentWeather(searchedCity);
-        getIndex(searchedCity);
-        getForecast(searchedCity);
-        addHistory(searchedCity);
-    })
-});
 
 
 //function to GET the current weather
